dae9e05435007cbffca94e7d6bacb3a5706c173b
Chandler Carruth <chandlerc@gmail.com>
2023-05-09 10:19:12 -0600

    format

--- scripts/gen_carbon.py
+++ scripts/gen_carbon.py
@@ -18,28 +18,28 @@ NUM_FIELDS = 10
 
 def random_id() -> str:
     length = random.randrange(3, 24)
-    return ''.join(random.choice(string.ascii_letters) for i in range(length))
+    return "".join(random.choice(string.ascii_letters) for i in range(length))
 
 
 def emit_field(i: int, k: int) -> None:
     name = random_id() + str(k)
-    print(f'  var {name}: i32;')
+    print(f"  var {name}: i32;")
 
 
 def emit_method(i: int, j: int) -> None:
     name = random_id() + str(j)
-    print(f'  // The {name} method declaration!')
+    print(f"  // The {name} method declaration!")
     num_params = random.randrange(0, 4)
-    params = ''
+    params = ""
     for p in range(num_params):
         param_name = random_id() + str(p)
         params += f'{"" if p == 0 else ", "}{param_name}: i32'
-    print(f'  fn {name}[self: Self]();')
+    print(f"  fn {name}[self: Self]();")
 
 
 def emit_class(i: int) -> None:
     name = random_id() + str(i)
-    print(f'class {name} {{')
+    print(f"class {name} {{")
 
     for j in range(NUM_METHODS):
         emit_method(i, j)
@@ -49,7 +49,7 @@ def emit_class(i: int) -> None:
     for k in range(NUM_FIELDS):
         emit_field(i, k)
 
-    print('}')
+    print("}")
 
 
 for i in range(NUM_CLASSES):
