abf283ec509f704aed5a98d2ec1d30bbe4be5bcc
Chandler Carruth <chandlerc@gmail.com>
2023-11-08 01:55:48 +0000

    and more cleaning

--- common/map_benchmark.cpp
+++ common/map_benchmark.cpp
@@ -7,13 +7,19 @@
 #include <type_traits>
 
 #include "absl/container/flat_hash_map.h"
-#include "absl/random/random.h"
 #include "common/map.h"
+#include "common/raw_hashtable_benchmark_helpers.h"
 #include "llvm/ADT/DenseMap.h"
 #include "llvm/ADT/SmallVector.h"
 
-namespace Carbon::Testing {
+namespace Carbon {
 namespace {
+
+using RawHashtable::BuildKeys;
+using RawHashtable::BuildShuffledKeys;
+using RawHashtable::CarbonHashingDenseInfo;
+using RawHashtable::NumOtherKeys;
+using RawHashtable::NumShuffledKeys;
 
 // This map has an intentional inlining blocker to avoid code growth. However,
 // both Abseil and LLVM's maps don't have this and at least on AArch64 both
@@ -98,166 +104,7 @@ struct MapWrapper<Map<KT, VT, MinSmallSize>> {
   auto BenchErase(KeyT k) -> bool { return M.Erase(k); }
 };
 
-template <typename T>
-struct CarbonHashingDenseInfo;
-
-template <> struct CarbonHashingDenseInfo<int> {
-  static inline auto getEmptyKey() -> int { return -1; }
-  static inline auto getTombstoneKey() -> int { return -2; }
-  static auto getHashValue(const int val) -> unsigned {
-    return static_cast<uint64_t>(HashValue(val));
-  }
-  static auto isEqual(const int lhs, const int rhs) -> bool {
-    return lhs == rhs;
-  }
-};
-
-template <typename T> struct CarbonHashingDenseInfo<T*> {
-  static constexpr uintptr_t Log2MaxAlign = 12;
-
-  static inline auto getEmptyKey() -> T* {
-    auto val = static_cast<uintptr_t>(-1);
-    val <<= Log2MaxAlign;
-    // NOLINTNEXTLINE(performance-no-int-to-ptr): This is required by the API.
-    return reinterpret_cast<int*>(val);
-  }
-
-  static inline auto getTombstoneKey() -> T* {
-    auto val = static_cast<uintptr_t>(-2);
-    val <<= Log2MaxAlign;
-    // NOLINTNEXTLINE(performance-no-int-to-ptr): This is required by the API.
-    return reinterpret_cast<int*>(val);
-  }
-
-  static auto getHashValue(const T* ptr_val) -> unsigned {
-    return static_cast<uint64_t>(HashValue(ptr_val));
-  }
-
-  static auto isEqual(const T* lhs, const T* rhs) -> bool {
-    return lhs == rhs;
-  }
-};
-
-template <> struct CarbonHashingDenseInfo<llvm::StringRef> {
-  static auto getEmptyKey() -> llvm::StringRef {
-    return llvm::StringRef(
-        // NOLINTNEXTLINE(performance-no-int-to-ptr): Required by the API.
-        reinterpret_cast<const char*>(~static_cast<uintptr_t>(0)), 0);
-  }
-
-  static auto getTombstoneKey() -> llvm::StringRef {
-    return llvm::StringRef(
-        // NOLINTNEXTLINE(performance-no-int-to-ptr): Required by the API.
-        reinterpret_cast<const char*>(~static_cast<uintptr_t>(1)), 0);
-  }
-  static auto getHashValue(llvm::StringRef val) -> unsigned {
-    return static_cast<uint64_t>(HashValue(val));
-  }
-  static bool isEqual(llvm::StringRef lhs, llvm::StringRef rhs) {
-    if (rhs.data() == getEmptyKey().data()) {
-      return lhs.data() == getEmptyKey().data();
-    }
-    if (rhs.data() == getTombstoneKey().data()) {
-      return lhs.data() == getTombstoneKey().data();
-    }
-    return lhs == rhs;
-  }
-};
-
-// We want to support benchmarking with 1M keys plus up to 1k "other" keys (for
-// misses).
-constexpr ssize_t NumOtherKeys = 1 << 10;
-constexpr ssize_t MaxNumKeys = (1 << 20) + NumOtherKeys;
-
-[[clang::noinline]] auto BuildStrKeys(ssize_t size)
-    -> llvm::ArrayRef<llvm::StringRef> {
-  static std::vector<llvm::StringRef> keys = [] {
-    std::vector<llvm::StringRef> keys;
-
-    // For benchmarking, we use short strings in a fixed distribution with
-    // common characters. Real-world strings aren't uniform across ASCII or
-    // Unicode, etc. And for *micro*-benchmarking we want to focus on the map
-    // overhead with short, fast keys.
-    std::vector<char> characters = {' ', '_', '-', '\n', '\t'};
-    for (auto range :
-         {llvm::seq_inclusive('a', 'z'), llvm::seq_inclusive('A', 'Z'),
-          llvm::seq_inclusive('0', '9')}) {
-      for (char c : range) {
-        characters.push_back(c);
-      }
-    }
-    ssize_t length_buckets[] = {
-        4, 4, 4, 4, 5, 5, 5, 5, 7, 7, 10, 10, 15, 25, 40, 80,
-    };
-
-    absl::BitGen gen;
-    keys.reserve(MaxNumKeys);
-    for (ssize_t i : llvm::seq<ssize_t>(0, MaxNumKeys)) {
-      // We allocate and leak a string for each key. This is fine as we're a
-      // static initializer only.
-      std::string& s = *new std::string();
-
-      ssize_t bucket = i % 16;
-      ssize_t length = length_buckets[bucket];
-      for ([[maybe_unused]] ssize_t j : llvm::seq<ssize_t>(0, length)) {
-        s.push_back(
-            characters[absl::Uniform<ssize_t>(gen, 0, characters.size())]);
-      }
-      keys.push_back(s);
-    }
-    return keys;
-  }();
-  return llvm::ArrayRef(keys).slice(0, size);
-}
-
-[[clang::noinline]] auto BuildPtrKeys(ssize_t size) -> llvm::ArrayRef<int*> {
-  static std::vector<int*> keys = [] {
-    std::vector<int*> keys;
-    for (ssize_t i : llvm::seq<ssize_t>(0, MaxNumKeys)) {
-      // We leak these pointers -- this is a static initializer executed once.
-      keys.push_back(new int(i));
-    }
-    return keys;
-  }();
-  return llvm::ArrayRef(keys).slice(0, size);
-}
-
-[[clang::noinline]] auto BuildIntKeys(ssize_t size) -> llvm::ArrayRef<int> {
-  static std::vector<int> keys = [] {
-    std::vector<int> keys;
-    for (ssize_t i : llvm::seq<ssize_t>(0, MaxNumKeys)) {
-      keys.push_back(i);
-    }
-    return keys;
-  }();
-  return llvm::ArrayRef(keys).slice(0, size);
-}
-
-template <typename T> auto BuildKeys(ssize_t size) -> llvm::ArrayRef<T> {
-  CARBON_CHECK(size <= MaxNumKeys);
-  if constexpr (std::is_same_v<T, llvm::StringRef>) {
-    return BuildStrKeys(size);
-  } else if constexpr (std::is_pointer_v<T>) {
-    return BuildPtrKeys(size);
-  } else {
-    return BuildIntKeys(size);
-  }
-}
-
-// 64k shuffled keys to avoid any easily detectable pattern.
-constexpr ssize_t NumShuffledKeys = 64 << 10;
-
-template <typename T>
-[[clang::noinline]] auto BuildShuffledKeys(llvm::ArrayRef<T> keys)
-    -> llvm::SmallVector<T> {
-  llvm::SmallVector<T> shuffled_keys;
-  for ([[maybe_unused]] ssize_t i : llvm::seq<ssize_t>(0, NumShuffledKeys)) {
-    shuffled_keys.push_back(keys[i % keys.size()]);
-  }
-  std::shuffle(shuffled_keys.begin(), shuffled_keys.end(), absl::BitGen());
-  return shuffled_keys;
-}
-
+// Helper to synthesize some value of one of the three types we use as value types.
 template <typename T>
 auto MakeValue2() -> T {
   if constexpr (std::is_same_v<T, llvm::StringRef>) {
@@ -433,11 +280,11 @@ static void BM_MapEraseUpdateHit(benchmark::State& s) {
     benchmark::ClobberMemory();
     bool inserted = m.BenchUpdate(shuffled_keys[i], MakeValue2<VT>());
     CARBON_DCHECK(inserted);
-    benchmark::DoNotOptimize(inserted);
     i = (i + static_cast<ssize_t>(inserted)) & (NumShuffledKeys - 1);
+    benchmark::DoNotOptimize(i);
   }
 }
 MAP_BENCHMARK_ONE_OP(BM_MapEraseUpdateHit);
 
 }  // namespace
-}  // namespace Carbon::Testing
+}  // namespace Carbon
--- common/set_benchmark.cpp
+++ common/set_benchmark.cpp
@@ -4,16 +4,20 @@
 
 #include <benchmark/benchmark.h>
 
-#include <memory>
-#include <random>
-
 #include "absl/container/flat_hash_set.h"
 #include "common/set.h"
+#include "common/raw_hashtable_benchmark_helpers.h"
 #include "llvm/ADT/DenseSet.h"
 #include "llvm/ADT/SmallVector.h"
 
 namespace Carbon {
 namespace {
+
+using RawHashtable::BuildKeys;
+using RawHashtable::BuildShuffledKeys;
+using RawHashtable::CarbonHashingDenseInfo;
+using RawHashtable::NumOtherKeys;
+using RawHashtable::NumShuffledKeys;
 
 // This map has an intentional inlining blocker to avoid code growth. However,
 // both Abseil and LLVM's maps don't have this and at least on AArch64 both
@@ -28,7 +32,26 @@ namespace {
 #endif
 
 template <typename SetT>
-struct SetWrapper;
+struct SetWrapper {
+  using KeyT = typename SetT::key_type;
+
+  SetT M;
+
+  void CreateView() {}
+
+  CARBON_ARM64_NOINLINE
+  auto BenchContains(KeyT k) -> bool { return M.find(k) != M.end(); }
+
+  CARBON_ARM64_NOINLINE
+  auto BenchInsert(KeyT k) -> bool {
+    auto result = M.insert(k);
+    benchmark::DoNotOptimize(result.second);
+    return result.second;
+  }
+
+  CARBON_ARM64_NOINLINE
+  auto BenchErase(KeyT k) -> bool { return M.erase(k) != 0; }
+};
 
 template <typename KT, int MinSmallSize>
 struct SetWrapper<Set<KT, MinSmallSize>> {
@@ -52,146 +75,6 @@ struct SetWrapper<Set<KT, MinSmallSize>> {
   auto BenchErase(KeyT k) -> bool { return M.Erase(k); }
 };
 
-template <typename KT, typename HasherT>
-struct SetWrapper<absl::flat_hash_set<KT, HasherT>> {
-  using SetT = absl::flat_hash_set<KT, HasherT>;
-  using KeyT = KT;
-
-  SetT M;
-
-  void CreateView() {}
-
-  CARBON_ARM64_NOINLINE
-  auto BenchContains(KeyT k) -> bool { return M.find(k) != M.end(); }
-
-  CARBON_ARM64_NOINLINE
-  auto BenchInsert(KeyT k) -> bool {
-    auto result = M.insert(k);
-    benchmark::DoNotOptimize(result.second);
-    return result.second;
-  }
-
-  CARBON_ARM64_NOINLINE
-  auto BenchErase(KeyT k) -> bool { return M.erase(k) != 0; }
-};
-
-template <typename KT, typename HasherT>
-struct SetWrapper<llvm::DenseSet<KT, HasherT>> {
-  using SetT = llvm::DenseSet<KT, HasherT>;
-  using KeyT = KT;
-
-  SetT M;
-
-  void CreateView() {}
-
-  CARBON_ARM64_NOINLINE
-  auto BenchContains(KeyT k) -> bool { return M.find(k) != M.end(); }
-
-  CARBON_ARM64_NOINLINE
-  auto BenchInsert(KeyT k) -> bool {
-    auto result = M.insert(k);
-    benchmark::DoNotOptimize(result.second);
-    return result.second;
-  }
-
-  CARBON_ARM64_NOINLINE
-  auto BenchErase(KeyT k) -> bool { return M.erase(k) != 0; }
-};
-
-template <typename KT, unsigned SmallSize, typename HasherT>
-struct SetWrapper<llvm::SmallDenseSet<KT, SmallSize, HasherT>> {
-  using SetT = llvm::SmallDenseSet<KT, SmallSize, HasherT>;
-  using KeyT = KT;
-
-  SetT M;
-
-  void CreateView() {}
-
-  auto BenchContains(KeyT k) -> bool {
-    return benchmark::DoNotOptimize(M.find(k) != M.end());
-  }
-
-  auto BenchInsert(KeyT k) -> bool {
-    auto result = M.insert(k);
-    benchmark::DoNotOptimize(result.second);
-    return result.second;
-  }
-
-  auto BenchErase(KeyT k) -> bool { return M.erase(k) != 0; }
-};
-
-struct CarbonHashingDenseInfo {
-  // The following should hold, but it would require int to be complete:
-  // static_assert(alignof(int) <= (1 << Log2MaxAlign),
-  //               "DenseSet does not support pointer keys requiring more than "
-  //               "Log2MaxAlign bits of alignment");
-  static constexpr uintptr_t Log2MaxAlign = 12;
-
-  static inline auto getEmptyKey() -> int* {
-    auto val = static_cast<uintptr_t>(-1);
-    val <<= Log2MaxAlign;
-    // NOLINTNEXTLINE(performance-no-int-to-ptr): This is required by the API.
-    return reinterpret_cast<int*>(val);
-  }
-
-  static inline auto getTombstoneKey() -> int* {
-    auto val = static_cast<uintptr_t>(-2);
-    val <<= Log2MaxAlign;
-    // NOLINTNEXTLINE(performance-no-int-to-ptr): This is required by the API.
-    return reinterpret_cast<int*>(val);
-  }
-
-  static auto getHashValue(const int* ptr_val) -> unsigned {
-    return static_cast<uint64_t>(HashValue(ptr_val));
-  }
-
-  static auto isEqual(const int* lhs, const int* rhs) -> bool {
-    return lhs == rhs;
-  }
-};
-
-using KeyVectorT = llvm::ArrayRef<std::unique_ptr<int>>;
-
-[[clang::noinline]] auto BuildKeys(
-    ssize_t size, llvm::function_ref<void(int*)> callback = [](int*) {})
-    -> KeyVectorT {
-  constexpr ssize_t MaxKeysSize = 1 << 20;
-  static std::vector<std::unique_ptr<int>>& keys = *([] {
-    auto* keys_ptr = new std::vector<std::unique_ptr<int>>();
-    for (ssize_t i : llvm::seq<ssize_t>(0, MaxKeysSize)) {
-      keys_ptr->emplace_back(new int(i));
-    }
-    return keys_ptr;
-  }());
-
-  for (ssize_t i : llvm::seq<ssize_t>(0, size)) {
-    callback(keys[i].get());
-  }
-
-  return llvm::ArrayRef(keys).take_front(size);
-}
-
-constexpr ssize_t NumShuffledKeys = 1024LL * 64;
-
-[[clang::noinline]] auto BuildShuffledKeys(const KeyVectorT& keys)
-    -> llvm::SmallVector<int*, 0> {
-  std::random_device r_dev;
-  std::seed_seq seed(
-      {r_dev(), r_dev(), r_dev(), r_dev(), r_dev(), r_dev(), r_dev(), r_dev()});
-  std::mt19937_64 rng(seed);
-  std::uniform_int_distribution<int> d(0, keys.size() - 1);
-
-  llvm::SmallVector<int*, 32> shuffled_keys;
-  for (ssize_t i : llvm::seq<ssize_t>(0, NumShuffledKeys)) {
-    (void)i;
-    ssize_t random_idx = d(rng);
-    assert(random_idx < (ssize_t)keys.size() && "Too large value!");
-    shuffled_keys.push_back(keys[random_idx].get());
-  }
-
-  return shuffled_keys;
-}
-
 static void OneOpSizeArgs(benchmark::internal::Benchmark* b) {
   b->DenseRange(1, 8, 1);
   b->DenseRange(12, 16, 4);
@@ -200,33 +83,39 @@ static void OneOpSizeArgs(benchmark::internal::Benchmark* b) {
 }
 
 // NOLINTBEGIN(bugprone-macro-parentheses): Parentheses are incorrect here.
-#define MAP_BENCHMARK_ONE_OP(NAME)                             \
-  BENCHMARK(NAME<Set<int*>>)->Apply(OneOpSizeArgs);                 \
-  BENCHMARK(NAME<absl::flat_hash_set<int*>>)->Apply(OneOpSizeArgs); \
-  BENCHMARK(NAME<llvm::DenseSet<int*, CarbonHashingDenseInfo>>)    \
+#define MAP_BENCHMARK_ONE_OP_SIZE(NAME, KT)                       \
+  BENCHMARK(NAME<Set<KT>>)->Apply(OneOpSizeArgs);                 \
+  BENCHMARK(NAME<absl::flat_hash_set<KT>>)->Apply(OneOpSizeArgs); \
+  BENCHMARK(NAME<llvm::DenseSet<KT, CarbonHashingDenseInfo<KT>>>) \
       ->Apply(OneOpSizeArgs)
 // NOLINTEND(bugprone-macro-parentheses)
+
+#define MAP_BENCHMARK_ONE_OP(NAME) MAP_BENCHMARK_ONE_OP_SIZE(NAME, int*)
 
 template <typename SetT>
 static void BM_SetContainsHitPtr(benchmark::State& s) {
   using SetWrapperT = SetWrapper<SetT>;
+  using KT = typename SetWrapperT::KeyT;
   SetWrapperT m;
-  KeyVectorT keys =
-      BuildKeys(s.range(0), [&m](int* key) { m.BenchInsert(key); });
-  llvm::SmallVector<int*, 32> shuffled_keys = BuildShuffledKeys(keys);
+  llvm::ArrayRef<KT> keys = BuildKeys<KT>(s.range(0));
+  for (auto k : keys) {
+    m.BenchInsert(k);
+  }
+  llvm::SmallVector<KT> shuffled_keys = BuildShuffledKeys(keys);
+  ssize_t i = 0;
 
   m.CreateView();
-
-  ssize_t i = 0;
   for (auto _ : s) {
     bool result = m.BenchContains(shuffled_keys[i]);
-    assert(result && "Should hit!");
-    benchmark::DoNotOptimize(result);
-    // We use `result` to step through the keys to establish a dependence
-    // between each iteration of the loop and allow the benchmark to measure
-    // *latency* rather than *throughput* -- the common case is to *do*
-    // something with the result of a lookup.
+    CARBON_DCHECK(result);
+    // We use the lookup success to step through keys, establishing a dependency
+    // between each lookup and allowing us to measure latency rather than
+    // throughput.
     i = (i + static_cast<ssize_t>(result)) & (NumShuffledKeys - 1);
+    // We block optimizing `i` as that has proven both more effective at
+    // blocking the loop from being optimized away and avoiding disruption of
+    // the generated code that we're benchmarking.
+    benchmark::DoNotOptimize(i);
   }
 }
 MAP_BENCHMARK_ONE_OP(BM_SetContainsHitPtr);
@@ -234,24 +123,22 @@ MAP_BENCHMARK_ONE_OP(BM_SetContainsHitPtr);
 template <typename SetT>
 static void BM_SetContainsMissPtr(benchmark::State& s) {
   using SetWrapperT = SetWrapper<SetT>;
+  using KT = typename SetWrapperT::KeyT;
   SetWrapperT m;
-  KeyVectorT keys =
-      BuildKeys(s.range(0), [&m](int* key) { m.BenchInsert(key); });
-  constexpr ssize_t NumOtherKeys = 1024LL * 64;
-  KeyVectorT other_keys = BuildKeys(NumOtherKeys);
+  llvm::ArrayRef<KT> keys = BuildKeys<KT>(s.range(0) + NumOtherKeys);
+  for (auto k : keys.slice(0, s.range(0))) {
+    m.BenchInsert(k);
+  }
+  llvm::SmallVector<KT> shuffled_keys =
+      BuildShuffledKeys(keys.slice(s.range(0)));
+  ssize_t i = 0;
 
   m.CreateView();
-
-  ssize_t i = 0;
   for (auto _ : s) {
-    bool result = m.BenchContains(other_keys[i].get());
-    assert(!result && "Should miss!");
-    benchmark::DoNotOptimize(result);
-    // We use `result` to step through the keys to establish a dependence
-    // between each iteration of the loop and allow the benchmark to measure
-    // *latency* rather than *throughput* -- the common case is to *do*
-    // something with the result of a lookup.
-    i = (i + static_cast<ssize_t>(!result)) & (NumOtherKeys - 1);
+    bool result = m.BenchContains(shuffled_keys[i]);
+    CARBON_DCHECK(!result);
+    i = (i + static_cast<ssize_t>(!result)) & (NumShuffledKeys - 1);
+    benchmark::DoNotOptimize(i);
   }
 }
 MAP_BENCHMARK_ONE_OP(BM_SetContainsMissPtr);
@@ -259,25 +146,23 @@ MAP_BENCHMARK_ONE_OP(BM_SetContainsMissPtr);
 template <typename SetT>
 static void BM_SetEraseInsertHitPtr(benchmark::State& s) {
   using SetWrapperT = SetWrapper<SetT>;
+  using KT = typename SetWrapperT::KeyT;
   SetWrapperT m;
-  KeyVectorT keys =
-      BuildKeys(s.range(0), [&m](int* key) { m.BenchInsert(key); });
-  llvm::SmallVector<int*, 32> shuffled_keys = BuildShuffledKeys(keys);
+  llvm::ArrayRef<KT> keys = BuildKeys<KT>(s.range(0));
+  for (auto k : keys) {
+    m.BenchInsert(k);
+  }
+  llvm::SmallVector<KT> shuffled_keys = BuildShuffledKeys(keys);
+  ssize_t i = 0;
 
   m.CreateView();
-
-  ssize_t i = 0;
   for (auto _ : s) {
     m.BenchErase(shuffled_keys[i]);
     benchmark::ClobberMemory();
     bool inserted = m.BenchInsert(shuffled_keys[i]);
-    assert(inserted && "Should insert!");
-    benchmark::DoNotOptimize(inserted);
-    // We use `inserted` to step through the keys to establish a dependence
-    // between each iteration of the loop and allow the benchmark to measure
-    // *latency* rather than *throughput* -- the common case is to *do*
-    // something with the result of a lookup.
+    CARBON_DCHECK(inserted);
     i = (i + static_cast<ssize_t>(inserted)) & (NumShuffledKeys - 1);
+    benchmark::DoNotOptimize(i);
   }
 }
 MAP_BENCHMARK_ONE_OP(BM_SetEraseInsertHitPtr);
@@ -291,36 +176,45 @@ static void OpSeqSizeArgs(benchmark::internal::Benchmark* b) {
 }
 
 // NOLINTBEGIN(bugprone-macro-parentheses): Parentheses are incorrect here.
-#define MAP_BENCHMARK_OP_SEQ(NAME)                             \
-  BENCHMARK(NAME<Set<int*>>)->Apply(OpSeqSizeArgs);                 \
-  BENCHMARK(NAME<absl::flat_hash_set<int*>>)->Apply(OpSeqSizeArgs); \
-  BENCHMARK(NAME<llvm::DenseSet<int*, CarbonHashingDenseInfo>>)    \
+#define MAP_BENCHMARK_OP_SEQ_SIZE(NAME, KT)                       \
+  BENCHMARK(NAME<Set<KT>>)->Apply(OpSeqSizeArgs);                 \
+  BENCHMARK(NAME<absl::flat_hash_set<KT>>)->Apply(OpSeqSizeArgs); \
+  BENCHMARK(NAME<llvm::DenseSet<KT, CarbonHashingDenseInfo<KT>>>) \
       ->Apply(OpSeqSizeArgs)
 // NOLINTEND(bugprone-macro-parentheses)
+
+#define MAP_BENCHMARK_OP_SEQ(NAME) MAP_BENCHMARK_OP_SEQ_SIZE(NAME, int*)
 
 template <typename SetT>
 static void BM_SetInsertPtrSeq(benchmark::State& s) {
   using SetWrapperT = SetWrapper<SetT>;
-  KeyVectorT keys = BuildKeys(s.range(0));
-  llvm::SmallVector<int*, 0> shuffled_keys = BuildShuffledKeys(keys);
+  using KT = typename SetWrapperT::KeyT;
+  llvm::ArrayRef<KT> raw_keys = BuildKeys<KT>(s.range(0));
+  // We want to permute the keys so they're not in any particular order, but not
+  // generate a full shuffled set. Note that the branch predictor is likely to
+  // still be able to learn the complete pattern of branches inserted for very
+  // small key ranges.
+  llvm::SmallVector<KT> keys(raw_keys.begin(), raw_keys.end());
+  std::shuffle(keys.begin(), keys.end(), absl::BitGen());
 
+  // Now build a large shuffled set of keys (with duplicates) we'll use at the
+  // end.
+  llvm::SmallVector<KT> shuffled_keys = BuildShuffledKeys(raw_keys);
   ssize_t i = 0;
   for (auto _ : s) {
-    // First insert all the keys.
     SetWrapperT m;
-    for (const auto& k : keys) {
-      bool inserted = m.BenchInsert(k.get());
-      assert(inserted && "Must be a successful insert!");
-      (void)inserted;
+    for (auto k : keys) {
+      bool inserted = m.BenchInsert(k);
+      CARBON_DCHECK(inserted) << "Must be a successful insert!";
     }
 
     // Now insert a final random repeated key.
     bool inserted = m.BenchInsert(shuffled_keys[i]);
-    assert(!inserted && "Must already be in the map!");
-    (void)inserted;
+    CARBON_DCHECK(!inserted) << "Must already be in the map!";
 
     // Rotate through the shuffled keys.
-    i = (i + 1) & (NumShuffledKeys - 1);
+    i = (i + static_cast<ssize_t>(inserted)) & (NumShuffledKeys - 1);
+    benchmark::DoNotOptimize(i);
   }
 }
 MAP_BENCHMARK_OP_SEQ(BM_SetInsertPtrSeq);
